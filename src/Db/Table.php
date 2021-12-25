5fd6866d643c646cc96ae830f8ef4bc74bf7e939
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2021-12-25 20:26:18 +0100

    Function refactoring.

--- src/Db/Table.php
+++ src/Db/Table.php
@@ -2,18 +2,17 @@
 
 namespace Lagdo\DbAdmin\Driver\MySql\Db;
 
-use Lagdo\DbAdmin\Driver\Entity\TableFieldEntity;
 use Lagdo\DbAdmin\Driver\Entity\TableEntity;
-use Lagdo\DbAdmin\Driver\Entity\IndexEntity;
-use Lagdo\DbAdmin\Driver\Entity\ForeignKeyEntity;
 use Lagdo\DbAdmin\Driver\Entity\TriggerEntity;
-
-use Lagdo\DbAdmin\Driver\Db\ConnectionInterface;
 
 use Lagdo\DbAdmin\Driver\Db\Table as AbstractTable;
 
 class Table extends AbstractTable
 {
+    use Traits\TableFieldTrait;
+    use Traits\TableIndexTrait;
+    use Traits\TableTrait;
+
     /**
      * @param bool $fast
      * @param string $table
@@ -91,141 +90,9 @@ class Table extends AbstractTable
     /**
      * @inheritDoc
      */
-    public function fields(string $table)
-    {
-        $fields = [];
-        foreach ($this->driver->rows("SHOW FULL COLUMNS FROM " . $this->driver->table($table)) as $row) {
-            preg_match('~^([^( ]+)(?:\((.+)\))?( unsigned)?( zerofill)?$~', $row["Type"], $match);
-            $match = array_pad($match, 5, '');
-
-            $field = new TableFieldEntity();
-
-            $field->name = $row["Field"];
-            $field->fullType = $row["Type"];
-            $field->type = $match[1];
-            $field->length = intval($match[2]);
-            $field->unsigned = ltrim($match[3] . $match[4]);
-            $field->default = ($row["Default"] != "" || preg_match("~char|set~", $match[1]) ?
-                (preg_match('~text~', $match[1]) ? stripslashes(preg_replace("~^'(.*)'\$~", '\1',
-                $row["Default"])) : $row["Default"]) : null);
-            $field->null = ($row["Null"] == "YES");
-            $field->autoIncrement = ($row["Extra"] == "auto_increment");
-            $field->onUpdate = (preg_match('~^on update (.+)~i', $row["Extra"], $match) ? $match[1] : ""); //! available since MySQL 5.1.23
-            $field->collation = $row["Collation"];
-            $field->privileges = array_flip(preg_split('~, *~', $row["Privileges"]));
-            $field->comment = $row["Comment"];
-            $field->primary = ($row["Key"] == "PRI");
-            // https://mariadb.com/kb/en/library/show-columns/
-            // https://github.com/vrana/adminer/pull/359#pullrequestreview-276677186
-            $field->generated = preg_match('~^(VIRTUAL|PERSISTENT|STORED)~', $row["Extra"]) > 0;
-
-            $fields[$field->name] = $field;
-        }
-        return $fields;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function supportForeignKeys(TableEntity $tableStatus)
-    {
-        return preg_match('~InnoDB|IBMDB2I~i', $tableStatus->engine)
-            || (preg_match('~NDB~i', $tableStatus->engine) && $this->driver->minVersion(5.6));
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function referencableTables(string $table)
-    {
-        $fields = []; // table_name => [field]
-        foreach ($this->tableStatuses(true) as $tableName => $tableStatus) {
-            if ($tableName === $table || !$this->supportForeignKeys($tableStatus)) {
-                continue;
-            }
-            foreach ($this->fields($tableName) as $field) {
-                if ($field->primary) {
-                    if (!isset($fields[$tableName])) {
-                        $fields[$tableName] = $field;
-                    } else {
-                        // No multi column primary key
-                        $fields[$tableName] = null;
-                    }
-                }
-            }
-        }
-        return array_filter($fields, function($field) {
-            return $field !== null;
-        });
-    }
-
-    /**
-     * @inheritDoc
-     */
     public function isView(TableEntity $tableStatus)
     {
         return $tableStatus->engine === null;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function indexes(string $table, ConnectionInterface $connection = null)
-    {
-        $indexes = [];
-        foreach ($this->driver->rows("SHOW INDEX FROM " . $this->driver->table($table), $connection) as $row) {
-            $index = new IndexEntity();
-
-            $name = $row["Key_name"];
-            $index->type = ($name == "PRIMARY" ? "PRIMARY" :
-                ($row["Index_type"] == "FULLTEXT" ? "FULLTEXT" : ($row["Non_unique"] ?
-                ($row["Index_type"] == "SPATIAL" ? "SPATIAL" : "INDEX") : "UNIQUE")));
-            $index->columns[] = $row["Column_name"];
-            $index->lengths[] = ($row["Index_type"] == "SPATIAL" ? null : $row["Sub_part"]);
-            $index->descs[] = null;
-
-            $indexes[$name] = $index;
-        }
-        return $indexes;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function foreignKeys(string $table)
-    {
-        static $pattern = '(?:`(?:[^`]|``)+`|"(?:[^"]|"")+")';
-        $foreignKeys = [];
-        $onActions = $this->driver->actions();
-        $create_table = $this->connection->result("SHOW CREATE TABLE " . $this->driver->table($table), 1);
-        if ($create_table) {
-            preg_match_all("~CONSTRAINT ($pattern) FOREIGN KEY ?\\(((?:$pattern,? ?)+)\\) REFERENCES " .
-                "($pattern)(?:\\.($pattern))? \\(((?:$pattern,? ?)+)\\)(?: ON DELETE ($onActions))" .
-                "?(?: ON UPDATE ($onActions))?~", $create_table, $matches, PREG_SET_ORDER);
-
-            foreach ($matches as $match) {
-                $match = array_pad($match, 8, '');
-
-                preg_match_all("~$pattern~", $match[2], $source);
-                preg_match_all("~$pattern~", $match[5], $target);
-
-                $foreignKey = new ForeignKeyEntity();
-
-                $foreignKey->database = $this->driver->unescapeId($match[4] != "" ? $match[3] : $match[4]);
-                $foreignKey->table = $this->driver->unescapeId($match[4] != "" ? $match[4] : $match[3]);
-                $foreignKey->source = array_map(function ($idf) {
-                    return $this->driver->unescapeId($idf);
-                }, $source[0]);
-                $foreignKey->target = array_map(function ($idf) {
-                    return $this->driver->unescapeId($idf);
-                }, $target[0]);
-                $foreignKey->onDelete = $match[6] ?: "RESTRICT";
-                $foreignKey->onUpdate = $match[7] ?: "RESTRICT";
-
-                $foreignKeys[$this->driver->unescapeId($match[1])] = $foreignKey;
-            }
-        }
-        return $foreignKeys;
     }
 
     /**
