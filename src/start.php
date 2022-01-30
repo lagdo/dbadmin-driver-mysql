28f34f5c1ae752c4e39c4e7dfff5b9b298abb2da
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2022-01-30 01:05:07 +0100

    Remove dependency on the jaxon-core library.

--- src/start.php
+++ src/start.php
@@ -1,11 +1,13 @@
 <?php
 
-$di = \jaxon()->di();
-// Register the database classes in the dependency container
-$di->set(Lagdo\DbAdmin\Driver\MySql\Driver::class, function($di) {
-    $util = $di->get(Lagdo\DbAdmin\Driver\UtilInterface::class);
-    $trans = $di->get(Lagdo\DbAdmin\Driver\TranslatorInterface::class);
-    $options = $di->get('dbadmin_config_options');
-    return new Lagdo\DbAdmin\Driver\MySql\Driver($util, $trans, $options);
-});
-$di->alias('dbadmin_driver_mysql', Lagdo\DbAdmin\Driver\MySql\Driver::class);
+if (function_exists('jaxon')) {
+    $di = jaxon()->di();
+    // Register the database classes in the dependency container
+    $di->set(Lagdo\DbAdmin\Driver\MySql\Driver::class, function($di) {
+        $util = $di->get(Lagdo\DbAdmin\Driver\UtilInterface::class);
+        $trans = $di->get(Lagdo\DbAdmin\Driver\TranslatorInterface::class);
+        $options = $di->get('dbadmin_config_options');
+        return new Lagdo\DbAdmin\Driver\MySql\Driver($util, $trans, $options);
+    });
+    $di->alias('dbadmin_driver_mysql', Lagdo\DbAdmin\Driver\MySql\Driver::class);
+}
