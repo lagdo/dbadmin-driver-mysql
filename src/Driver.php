f87e5bb612c618cc0f5097872220c7f939fc9161
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2024-02-08 04:41:12 +0100

    Improved the driver initialization.

--- src/Driver.php
+++ src/Driver.php
@@ -9,76 +9,56 @@ use Lagdo\DbAdmin\Driver\Db\Connection as AbstractConnection;
 class Driver extends AbstractDriver
 {
     /**
-     * Features not available
-     *
-     * @var array
+     * @inheritDoc
      */
-    private $features = ['scheme', 'sequence', 'type', 'view_trigger', 'materializedview'];
+    protected function initDriver()
+    {
+        // Init config
+        $this->config->jush = 'sql';
+        $this->config->drivers = ["MySQLi", "PDO_MySQL"];
+        $this->config->setTypes([
+            'Numbers' => ["tinyint" => 3, "smallint" => 5, "mediumint" => 8, "int" => 10,
+                "bigint" => 20, "decimal" => 66, "float" => 12, "double" => 21],
+            'Date and time' => ["date" => 10, "datetime" => 19, "timestamp" => 19, "time" => 10, "year" => 4],
+            'Strings' => ["char" => 255, "varchar" => 65535, "tinytext" => 255,
+                "text" => 65535, "mediumtext" => 16777215, "longtext" => 4294967295],
+            'Lists' => ["enum" => 65535, "set" => 64],
+            'Binary' => ["bit" => 20, "binary" => 255, "varbinary" => 65535, "tinyblob" => 255,
+                "blob" => 65535, "mediumblob" => 16777215, "longblob" => 4294967295],
+            'Geometry' => ["geometry" => 0, "point" => 0, "linestring" => 0, "polygon" => 0,
+                "multipoint" => 0, "multilinestring" => 0, "multipolygon" => 0, "geometrycollection" => 0],
+        ]);
+        $this->config->unsigned = ["unsigned", "zerofill", "unsigned zerofill"];
+        $this->config->operators = ["=", "<", ">", "<=", ">=", "!=", "LIKE", "LIKE %%",
+            "REGEXP", "IN", "FIND_IN_SET", "IS NULL", "NOT LIKE", "NOT REGEXP",
+            "NOT IN", "IS NOT NULL", "SQL"];
+        $this->config->functions = ["char_length", "date", "from_unixtime", "lower",
+            "round", "floor", "ceil", "sec_to_time", "time_to_sec", "upper"];
+        $this->config->grouping = ["avg", "count", "count distinct", "group_concat", "max", "min", "sum"];
+        $this->config->editFunctions = [[
+            "char" => "md5/sha1/password/encrypt/uuid",
+            "binary" => "md5/sha1",
+            "date|time" => "now",
+        ],[
+            $this->numberRegex() => "+/-",
+            "date" => "+ interval/- interval",
+            "time" => "addtime/subtime",
+            "char|text" => "concat",
+        ]];
+        /**
+         * Features not available
+         *
+         * @var array
+         */
+        $this->config->features = ['database', 'table', 'columns', 'sql', 'indexes', 'descidx',
+            'comment', 'processlist', 'variables', 'drop_col', 'kill', 'dump', 'fkeys_sql'];
 
-    /**
-     * Data types
-     *
-     * @var array
-     */
-    private $types = [
-        'Numbers' => ["tinyint" => 3, "smallint" => 5, "mediumint" => 8, "int" => 10,
-            "bigint" => 20, "decimal" => 66, "float" => 12, "double" => 21],
-        'Date and time' => ["date" => 10, "datetime" => 19, "timestamp" => 19, "time" => 10, "year" => 4],
-        'Strings' => ["char" => 255, "varchar" => 65535, "tinytext" => 255,
-            "text" => 65535, "mediumtext" => 16777215, "longtext" => 4294967295],
-        'Lists' => ["enum" => 65535, "set" => 64],
-        'Binary' => ["bit" => 20, "binary" => 255, "varbinary" => 65535, "tinyblob" => 255,
-            "blob" => 65535, "mediumblob" => 16777215, "longblob" => 4294967295],
-        'Geometry' => ["geometry" => 0, "point" => 0, "linestring" => 0, "polygon" => 0,
-            "multipoint" => 0, "multilinestring" => 0, "multipolygon" => 0, "geometrycollection" => 0],
-    ];
-
-    /**
-     * Number variants
-     *
-     * @var array
-     */
-    private $unsigned = ["unsigned", "zerofill", "unsigned zerofill"];
-
-    /**
-     * Operators used in select
-     *
-     * @var array
-     */
-    private $operators = ["=", "<", ">", "<=", ">=", "!=", "LIKE", "LIKE %%",
-        "REGEXP", "IN", "FIND_IN_SET", "IS NULL", "NOT LIKE", "NOT REGEXP",
-        "NOT IN", "IS NOT NULL", "SQL"];
-
-    /**
-     * Functions used in select
-     *
-     * @var array
-     */
-    private $functions = ["char_length", "date", "from_unixtime", "lower",
-        "round", "floor", "ceil", "sec_to_time", "time_to_sec", "upper"];
-
-    /**
-     * Grouping functions used in select
-     *
-     * @var array
-     */
-    private $grouping = ["avg", "count", "count distinct", "group_concat", "max", "min", "sum"];
-
-    /**
-     * Functions used to edit data
-     *
-     * @var array
-     */
-    private $editFunctions = [[
-        "char" => "md5/sha1/password/encrypt/uuid",
-        "binary" => "md5/sha1",
-        "date|time" => "now",
-    ],[
-        // $this->numberRegex() => "+/-",
-        "date" => "+ interval/- interval",
-        "time" => "addtime/subtime",
-        "char|text" => "concat",
-    ]];
+        $this->server = new Db\Server($this, $this->util, $this->trans);
+        $this->database = new Db\Database($this, $this->util, $this->trans);
+        $this->table = new Db\Table($this, $this->util, $this->trans);
+        $this->query = new Db\Query($this, $this->util, $this->trans);
+        $this->grammar = new Db\Grammar($this, $this->util, $this->trans);
+    }
 
     /**
      * @inheritDoc
@@ -97,14 +77,6 @@ class Driver extends AbstractDriver
      */
     private function initConnection(AbstractConnection $connection)
     {
-        if ($this->connection === null) {
-            $this->connection = $connection;
-            $this->server = new Db\Server($this, $this->util, $this->trans);
-            $this->database = new Db\Database($this, $this->util, $this->trans);
-            $this->table = new Db\Table($this, $this->util, $this->trans);
-            $this->query = new Db\Query($this, $this->util, $this->trans);
-            $this->grammar = new Db\Grammar($this, $this->util, $this->trans);
-        }
         // if (!$connection->open($this->options('server'), $this->options())) {
         //     $error = $this->error();
         //     // windows-1250 - most common Windows encoding
@@ -114,7 +86,7 @@ class Driver extends AbstractDriver
         //     }
         //     throw new AuthException($error);
         // }
-        return $connection;
+        return $this->connection = $connection;
     }
 
     /**
@@ -141,48 +113,22 @@ class Driver extends AbstractDriver
     {
         parent::connect($database, $schema);
 
-        if (!$this->minVersion(8)) {
-            $this->features[] = 'descidx';
-            if (!$this->minVersion(5.1)) {
-                $this->features[] = 'event';
-                $this->features[] = 'partitioning';
-                if (!$this->minVersion(5)) {
-                    $this->features[] = 'routine';
-                    $this->features[] = 'trigger';
-                    $this->features[] = 'view';
-                }
+        if ($this->minVersion(5)) {
+            $this->config->features[] = 'routine';
+            $this->config->features[] = 'trigger';
+            $this->config->features[] = 'view';
+            if ($this->minVersion(5.1)) {
+                $this->config->features[] = 'event';
+                $this->config->features[] = 'partitioning';
+            }
+            if ($this->minVersion(8)) {
+                $this->config->features[] = 'descidx';
             }
         }
-
         if ($this->minVersion('5.7.8', 10.2)) {
             $this->config->structuredTypes[$this->trans->lang('Strings')][] = "json";
             $this->config->types["json"] = 4294967295;
         }
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function support(string $feature)
-    {
-        // $this->features contains features that are not available.
-        return !in_array($feature, $this->features);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    protected function initConfig()
-    {
-        $this->config->jush = 'sql';
-        $this->config->drivers = ["MySQLi", "PDO_MySQL"];
-        $this->config->setTypes($this->types, $this->trans);
-        $this->config->unsigned = $this->unsigned;
-        $this->config->operators = $this->operators;
-        $this->config->functions = $this->functions;
-        $this->config->grouping = $this->grouping;
-        $this->config->editFunctions = $this->editFunctions;
-        $this->config->editFunctions[1][$this->numberRegex()] = "+/-";
     }
 
     /**
